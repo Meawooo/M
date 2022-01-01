7f61c6ccac61762048edd742ebb54ae5500b99c5
Meawooo <96882676+Meawooo@users.noreply.github.com>
2022-01-01 23:02:55 +0530

    Update neverhaveiever.js

--- Commands/Games/neverhaveiever.js
+++ Commands/Games/neverhaveiever.js
@@ -7,9 +7,9 @@ module.exports = {
 		await NeverHaveIEver({
 			message: message,
 			embed: {
-				title: 'Never Have I Ever | Weky Development',
+				title: 'Never Have I Ever | CLOMEL Development',
 				color: '#5865F2',
-				footer: 'Reaper Games',
+				footer: 'CLOMEL Games',
 				timestamp: true
 			},
 			thinkMessage: 'I am thinking',
